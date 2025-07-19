de352440037233eb44367762fe2393f527f4d4ee
ThaoHN <hoangngocthaonazca@baoviet.com.vn>
2025-07-19 22:47:08 +0700

    Create ProfileService

---
+++ profile-service/src/main/java/com/devteria/profile/mapper/UserProfileMapper.java
@@ -0,0 +1,15 @@
+package com.devteria.profile.mapper;
+
+import com.devteria.profile.dto.request.ProfileCreationRequest;
+import com.devteria.profile.dto.response.UserProfileResponse;
+import com.devteria.profile.entity.UserProfile;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserProfileMapper {
+    UserProfile toUserProfile(ProfileCreationRequest profileCreationRequest);
+    @Mapping(source = "id", target = "id")
+    UserProfileResponse toUserProfileResponse(UserProfile entity);
+
+}
---
+++ profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
@@ -0,0 +1,40 @@
+package com.devteria.profile.service;
+
+import com.devteria.profile.dto.request.ProfileCreationRequest;
+import com.devteria.profile.dto.response.UserProfileResponse;
+import com.devteria.profile.entity.UserProfile;
+import com.devteria.profile.mapper.UserProfileMapper;
+import com.devteria.profile.repository.UserProfileRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class UserProfileService {
+    UserProfileRepository userProfileRepository;
+    UserProfileMapper userProfileMapper;
+
+    public UserProfileResponse createProfile(ProfileCreationRequest request) {
+        UserProfile userProfile = userProfileMapper.toUserProfile(request);
+//        userProfile.setId(UUID.randomUUID().toString());
+        userProfile = userProfileRepository.save(userProfile);
+
+        return userProfileMapper.toUserProfileResponse(userProfile);
+    }
+
+    public UserProfileResponse getProfile(String id) {
+        UserProfile userProfile = userProfileRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User profile not found"));
+        return userProfileMapper.toUserProfileResponse(userProfile);
+
+    }
+
+
+}
